5a342a660ce1d4947930fa9e5c34882a422b46cb
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-01-16 23:59:07 +0530

    Add files via upload

--- que10.cpp
+++ que10.cpp
@@ -1,42 +1,41 @@
-// Draw the patter for n=3
-/*
-    21 22 23 24 25
-    20 7  8  9  10
-    19 6  1  2  11
-    18 5  4  3  12
-    17 16 15 14 13
-*/
-
 #include <stdio.h>
 
 int main() {
     int n;
-    printf("Enter the value of n:");
-    scanf("%d",&n);
-    int size = 2 * n + 1;
-    int matrix[size][size];
-    int num = size * size;
-    int left = 0, right = size - 1, top = 0, bottom = size - 1;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
 
-    while (left <= right && top <= bottom) {
-        for (int i = left; i <= right; i++) {
-            matrix[top][i] = num--;
+    int size = 2 * n - 1; // The size of the square matrix
+    int matrix[size][size];
+    int start = 1, end = size, num = size * size;
+
+    while (start <= end) {
+        // Fill the top row
+        for (int i = start - 1; i < end; i++) {
+            matrix[start - 1][i] = num--;
         }
-        top++;
-        for (int i = top; i <= bottom; i++) {
-            matrix[i][right] = num--;
+
+        // Fill the right column
+        for (int i = start; i < end; i++) {
+            matrix[i][end - 1] = num--;
         }
-        right--;
-        for (int i = right; i >= left; i--) {
-            matrix[bottom][i] = num--;
+
+        // Fill the bottom row
+        for (int i = end - 2; i >= start - 1; i--) {
+            matrix[end - 1][i] = num--;
         }
-        bottom--;
-        for (int i = bottom; i >= top; i--) {
-            matrix[i][left] = num--;
+
+        // Fill the left column
+        for (int i = end - 2; i >= start; i--) {
+            matrix[i][start - 1] = num--;
         }
-        left++;
+
+        // Move to the inner layer
+        start++;
+        end--;
     }
 
+    // Print the resulting matrix
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             printf("%4d", matrix[i][j]);
